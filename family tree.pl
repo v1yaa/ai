bf9cabe39ed48fa90476499eff5f13bd24b1256e
Viyaa <viyaa2608@gmail.com>
2025-05-29 08:24:47 +0530

    Update family tree.pl

--- family tree.pl
+++ family tree.pl
@@ -17,3 +17,13 @@ father(X, Y) :- male(X), parent(X, Y).
 mother(X, Y) :- female(X), parent(X, Y).
 sibling(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
 grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
+
+% output:
+?- father(john, paul).        
+true
+?- mother(mary, linda).       
+true
+?- sibling(paul, linda).      
+true
+?- grandparent(john, david).  
+true
