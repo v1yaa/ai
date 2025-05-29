e6a3d5b8f8fbc462c43ee3704db895adb0218f2c
Viyaa <viyaa2608@gmail.com>
2025-05-29 08:20:50 +0530

    Update fruit and color.pl

--- fruit and color.pl
+++ fruit and color.pl
@@ -4,3 +4,18 @@ fruit(grape, purple).
 fruit(orange, orange).
 fruit(kiwi, green).
 fruit(apple, green).
+
+% output:
+?- fruit(F, C).
+F = apple,
+C = red ;
+F = banana,
+C = yellow ;
+F = grape,
+C = purple ;
+F = C,
+C = orange ;
+F = kiwi,
+C = green ;
+F = apple,
+C = green ;
