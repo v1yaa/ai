759d2efdfc1757846e7b1c4eb33ad81fb5f702b5
Viyaa <viyaa2608@gmail.com>
2025-05-29 08:11:38 +0530

    Update count vowels.pl

--- count vowels.pl
+++ count vowels.pl
@@ -18,3 +18,13 @@ count_vowels([H|T], Count) :-
 count_vowels([H|T], Count) :-
     \+ vowel(H),
     count_vowels(T, Count).
+
+% output:
+?- count_vowels([h, e, l, l, o], Count).
+Count = 2.
+
+?- count_vowels([p, r, o, l, o, g], Count).
+Count = 2.
+
+?- count_vowels([], Count).
+Count = 0.
