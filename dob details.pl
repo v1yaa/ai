789ac88cc0a83ee8982ffeeb526e8d4e60564a8f
Viyaa <viyaa2608@gmail.com>
2025-05-29 08:33:04 +0530

    Update dob details.pl

--- dob details.pl
+++ dob details.pl
@@ -3,3 +3,16 @@ person('Bob', dob(23, june, 1990)).
 person('Charlie', dob(5, december, 1985)).
 person('Diana', dob(29, february, 2000)).
 person('Eve', dob(10, october, 1998)).
+
+% output:
+?- person(Name, DOB).
+Name = 'Alice',
+DOB = dob(12, may, 1995) ;
+Name = 'Bob',
+DOB = dob(23, june, 1990) ;
+Name = 'Charlie',
+DOB = dob(5, december, 1985) ;
+Name = 'Diana',
+DOB = dob(29, february, 2000) ;
+Name = 'Eve',
+DOB = dob(10, october, 1998).
